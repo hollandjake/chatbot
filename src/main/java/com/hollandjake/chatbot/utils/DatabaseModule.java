ef6d02fed6c6ea00198e0ba251ee502f4fdf1b0c
Jake Holland <hollandjake@hotmail.co.uk>
2019-06-26 15:17:42 +0100

    Added some missed data

--- src/main/java/com/hollandjake/chatbot/utils/DatabaseModule.java
+++ src/main/java/com/hollandjake/chatbot/utils/DatabaseModule.java
@@ -1,5 +1,6 @@
 package com.hollandjake.chatbot.utils;
 
+import com.google.errorprone.annotations.ForOverride;
 import com.hollandjake.chatbot.Chatbot;
 import com.hollandjake.messengerBotAPI.util.DatabaseController;
 
@@ -10,25 +11,28 @@ import java.sql.SQLException;
 
 public abstract class DatabaseModule extends Module {
 	protected final DatabaseController db;
-	protected Integer MOD_ID;
+	protected Integer modId;
 
 	protected DatabaseModule(Chatbot chatbot) {
 		super(chatbot);
 		this.db = chatbot.getDb();
 	}
 
-	public abstract void prepareStatements(Connection connection) throws SQLException;
+	@ForOverride
+	public void prepareStatements(Connection connection) throws SQLException {
+		updateModuleId(connection);
+	}
 
-	public void updateModuleId(Connection connection) throws SQLException {
+	protected void updateModuleId(Connection connection) throws SQLException {
 		PreparedStatement GET_MOD_ID = connection.prepareStatement("SELECT module_id FROM module WHERE module_name = ?");
 		GET_MOD_ID.setString(1, getClass().getSimpleName());
 		ResultSet resultSet = GET_MOD_ID.executeQuery();
 		if (resultSet.next()) {
-			MOD_ID = resultSet.getInt("module_id");
+			modId = resultSet.getInt("module_id");
 		}
 	}
 
-	public Integer getMOD_ID() {
-		return MOD_ID;
+	public Integer getModId() {
+		return modId;
 	}
 }
